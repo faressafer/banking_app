f27f9263f3a80564fedc3e08c5c775cdd8c5efd9
FaresOps <faressafer05@gmail.com>
2024-09-09 03:03:21 +0100

    Side Bar Prep

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const page = () => {
+
+
+  const loggedIn = { firstName :' Fares'}
   return (
     <div>
-      Home
+      <section className='home'>
+        <div className='home-content'>
+          <header className='home-header'>
+              <HeaderBox
+              type = "greeting"
+              title="welcome"
+              user= {loggedIn?.firstName || 'Guest'}
+              subtext="Acces and manage your account and transactions efficently."
+              />
+
+
+              <TotalBalanceBox
+              accounts ={[]}
+              totalBanks={2}
+              totalCurrentBalance={15245}
+              />
+
+          </header>
+        </div>
+      </section>
     </div>
   )
 }
